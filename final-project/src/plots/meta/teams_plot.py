fa5e4785de309661e0630ae094b78d59935bdeb8
silas <silas.hoffmann@stud.uni-goettingen.de>
2023-07-03 16:34:42 +0200

    improved comparison of active teams to games played

--- final-project/src/plots/meta/teams_plot.py
+++ final-project/src/plots/meta/teams_plot.py
@@ -23,23 +23,28 @@ def active_teams_per_year(df):
 
 
 def team_lifetimes(df):
-    # TODO implement this: Active teams per year vs. games played
-
+    data_per_year = df.groupby(['date_year']).size().to_dict()
     lifetime_data = calc_team_lifetimes(df)
+
+    fig, ax_plot = plt.subplots(figsize=(10, 5))
+    plt_with_disruption(ax_plot, data_per_year.keys(), data_per_year.values(), label='data rows per year')
     log.debug("team_lifetimes: %s", lifetime_data['team_lifetimes'])
 
-    fig, ax = plt.subplots()
-
+    ax_bar = ax_plot.twinx()
     team_lifetimes = lifetime_data['team_lifetimes']
     sorted_teams_by_first_year = lifetime_data['sorted_teams_by_first_year']
 
     for curr_team, curr_first_year in sorted_teams_by_first_year.items():
-        ax.barh(curr_team, width=team_lifetimes[curr_team], left=curr_first_year, color='blue', alpha=0.5)
+        ax_bar.barh(curr_team, width=team_lifetimes[curr_team], left=curr_first_year, color='gray', alpha=0.5)
 
-    active_teams_per_year = lifetime_data['active_teams_per_year']
-    plt_with_disruption(ax, active_teams_per_year.keys(), active_teams_per_year.values())
+    selected_teams = ['Boston - Braves']
+    ax_bar.set_yticks(range(len(sorted_teams_by_first_year)))
+    ax_bar.set_yticklabels([team if team in selected_teams else '' for team in sorted_teams_by_first_year.keys()])
 
-    ax.set_ylabel('active teams')
-    ax.set_xlabel('decade')
+    ax_plot.set_xlabel('decade')
+    ax_plot.set_ylabel('gmaes per year')
+    ax_bar.set_ylabel('team lifetimes')
     plt.title('Active teams per year vs. games played')
     plt.legend()
+
+    plt.subplots_adjust(right=0.8)
--- final-project/src/vis_main.py
+++ final-project/src/vis_main.py
@@ -5,14 +5,14 @@ from src.utils.graph_data_io import read_game_logs, export_graph
 
 df = read_game_logs()
 
-export_graph(score_plot.v_score_count, df)
-export_graph(score_plot.h_score_count, df)
-export_graph(score_plot.vh_score_comparison, df)
-export_graph(score_plot.winning_teams, df) # TODO hightlights seem a bit off
-
-export_graph(event_plot.data_per_year, df)
-export_graph(event_plot.attendance_per_year, df)
-export_graph(event_plot.games_per_year_per_team, df)
-
-export_graph(teams_plot.active_teams_per_year, df)
+# export_graph(score_plot.v_score_count, df)
+# export_graph(score_plot.h_score_count, df)
+# export_graph(score_plot.vh_score_comparison, df)
+# export_graph(score_plot.winning_teams, df) # TODO hightlights seem a bit off
+#
+# export_graph(event_plot.data_per_year, df)
+# export_graph(event_plot.attendance_per_year, df)
+# export_graph(event_plot.games_per_year_per_team, df)
+#
+# export_graph(teams_plot.active_teams_per_year, df)
 export_graph(teams_plot.team_lifetimes, df)
